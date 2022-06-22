ec250cbd43a0ab1f47df7d4ac3e51b122124a0b4
Greenz081081 <Blessededet94@gmail.com>
2022-06-22 14:01:59 +0100

    push

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -7,3 +7,27 @@ x.onclick = toggleMenu;
 
 const lastModified = (document.lastModified);
 document.querySelector("#currentdate").textContent = lastModified;
+
+const daysName = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+const day = new Date();
+const dayname = daysName[day.getDay()];
+
+function displayMessage () {
+
+    if (dayname === "Wednesday") {
+        document.getElementById("invitation").innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    } else if (dayname === "Tuesday") {
+        document.getElementById("invitation").innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    };
+};
+
+displayMessage();
