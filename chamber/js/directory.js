2fbe13a88296abf3749c02ddf0f3f24a3a8b153e
Greenz081081 <Blessededet94@gmail.com>
2022-06-24 16:43:48 +0100

    push

---
+++ chamber/js/directory.js
@@ -0,0 +1,48 @@
+const requestURL = 'chamber/data.json';
+const cards = document.querySelector('.cards');
+
+async function getCompanies () {
+  let response = await fetch(requestURL);
+  if (response.ok) {
+    let data = await response.json();
+    // console.log(data);
+    buildCompanyCards(data);
+  } else {
+    throw Error(response.statusText);
+  }
+}
+
+function buildCompanyCards(data) {
+  data.companies.forEach(company => {
+    let card = document.createElement("section");
+    let p = document.createElement("p");
+    let img = document.createElement("img");
+    let h2 = document.createElement("h2");
+    let h6 = document.createElement("h6");
+
+    let fullName = `${company.name}`;
+    let companyAddress = `${company.address}`;
+    let phoneNumber = `${company.phone}`;
+    let membershipLevel = `${company.membershiplevel}`;
+    let websiteUrl = `${company.websiteurl}`;
+    let numberOrder = `${company.order}`;
+
+    h2.innerHTML = fullName;
+    p.innerHTML = `Address: ${companyAddress}`;
+    p.innerHTML = `Website: ${websiteUrl}`;
+    h6.innerHTML = `Phone: ${phoneNumber}`;
+    p.innerHTML = `Membership: ${membershipLevel}`
+    img.setAttribute("src", company.image);
+    img.setAttribute("alt", `Image of ${fullName} - the ${numberOrder}th company`);
+    img.setAttribute("loading", "lazy");
+
+    card.append(h2);
+    card.append(h6)
+    card.appendChild(p);
+    card.append(img)
+
+    cards.append(card);
+  });
+}
+
+getCompanies();
