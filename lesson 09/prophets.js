88521e94c6b3a28da7eec90fb8fc714974bbf491
Greenz081081 <Blessededet94@gmail.com>
2022-06-24 11:52:13 +0100

    push

--- lesson 09/prophets.js
+++ lesson 09/prophets.js
@@ -1,39 +1,43 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-const cards = document.querySelector(".card");
+const cards = document.querySelector('.cards');
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    // prophets.forEach(displayProphets);
-  });
-
-
-
-const prophets = jsonObject["prophets"];
-
-function displayProphets(prophet) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-  
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    // h2.textContent = prophet.name + ' ' + prophet.lastname;
-    h2.textContent = `prophet.name prophet.lastname`;
-    console.log(h2.textContent)
-  
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', 'Portait of '  `prophet.name prophet.lastname`);
-    ____.setAttribute('loading', 'lazy');
-  
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(portrait);
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
+async function getProphets () {
+  let response = await fetch(requestURL);
+  if (response.ok) {
+    let data = await response.json();
+    // console.log(data);
+    buildProphetCards(data);
+  } else {
+    throw Error(response.statusText);
   }
+}
+
+function buildProphetCards(data) {
+  data.prophets.forEach(prophet => {
+    let card = document.createElement("section");
+    let p = document.createElement("p");
+    let img = document.createElement("img");
+    let h2 = document.createElement("h2");
+    let h6 = document.createElement("h6");
+
+    let fullName = `${prophet.name} ${prophet.lastname}`;
+    let birthDate = `Date of Birth: ${prophet.birthdate}`;
+    let numberOrder = `${prophet.order}`;
+
+    h2.innerHTML = fullName;
+    p.innerHTML = `Location of Birth: ${prophet.birthplace}`;
+    h6.innerHTML = birthDate;
+    img.setAttribute("src", prophet.imageurl);
+    img.setAttribute("alt", `Potrait of ${fullName} - ${numberOrder}th Latter-Day President`);
+    img.setAttribute("loading", "lazy");
+
+    card.append(h2);
+    card.append(h6)
+    card.appendChild(p);
+    card.append(img)
+
+    cards.append(card);
+  });
+}
+
+getProphets();
