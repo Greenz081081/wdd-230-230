8a816edee4d12cda538f60d5b24356eb90af4c03
Greenz081081 <Blessededet94@gmail.com>
2022-06-26 03:16:49 +0100

    push

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -50,3 +50,19 @@ function buildCompanyCards(data) {
 }
 
 getCompanies();
+
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridButton.addEventListener("click", () => {
+  display.classList.add("grid");
+  display.classList.remove("list");
+});
+
+listButton.addEventListener("click", showList);
+
+function showList() {
+  display.classList.add("list");
+  display.classList.remove("grid");
+}
