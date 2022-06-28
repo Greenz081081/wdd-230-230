52c1762ad50061e5e208b2af632cdade167c1946
Greenz081081 <Blessededet94@gmail.com>
2022-06-28 11:31:06 +0100

    push

--- lesson 10/js/weatherapi.js
+++ lesson 10/js/weatherapi.js
@@ -2,6 +2,8 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const sectiondesc = document.querySelector('section');
+
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&
 appid=cb411365f163ce2a1a5d327896469c7f`;
@@ -29,9 +31,27 @@ function displayResults(weatherData) {
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
+    const speed = weatherData.wind.speed;
+    const humidity = weatherData.main.humidity;
+    const temp = weatherData.main.temp;
+
+    let section = document.createElement("section");
+    let p = document.createElement("p");
+    let p2 = document.createElement("p");
+    let p3 = document.createElement("p");
+    
 
 
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.innerHTML = desc;
+    captionDesc.innerHTML = desc[0].toUpperCase() + desc.slice(1);
+    p.innerHTML = `<strong>Wind:</strong> ${speed.toFixed(1)}mph W`;
+    p2.innerHTML = `<strong>Humidity:</strong> ${humidity}%`;
+    p3.innerHTML = `<strong>Temp:</strong> ${temp}&deg;F`;
+
+    section.appendChild(p);
+    section.appendChild(p2);
+    section.appendChild(p3);
+
+    captionDesc.append(section);
 }
