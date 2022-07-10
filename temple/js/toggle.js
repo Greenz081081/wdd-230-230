97b628c98493aee776fc3b4e1f73a84d65032f02
Greenz081081 <Blessededet94@gmail.com>
2022-07-10 22:03:12 +0100

    push

--- temple/js/toggle.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu () {
-    document.querySelector("#primaryNav").classList.toggle("open");
-}
-
-const x = document.querySelector("#hamburgerButton");
-x.onclick = toggleMenu;
-
-const lastModified = (document.lastModified);
-document.querySelector("#currentdate").textContent = lastModified;
