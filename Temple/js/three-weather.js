060d733ce05a0700b288eae9f8f9d09ac8358433
Greenz081081 <Blessededet94@gmail.com>
2022-07-20 09:11:55 +0100

    push

--- Temple/js/three-weather.js
+++ Temple/js/three-weather.js
@@ -39,42 +39,15 @@ const dayname = daysName[day.getDay()];
 
 function displayThreeDayResults(weatherData) {
   currentTemperature.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-
-
-  const icon = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-  const des = weatherData.weather[0].description;
-  const speed = weatherData.wind.speed;
   const temp = weatherData.main.temp;
-  const humidity = weatherData.main.humidity;
+
 
   let section = document.createElement("section");
-  let image = document.createElement("img");
-  let h6 = document.createElement("h6");
-  let p = document.createElement("p");
-  let p2 = document.createElement("p");
   let p3 = document.createElement("p");
 
-  let result = 35.74 + 0.6215 * temp - 33.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16);
-  let description = des[0].toUpperCase() + des.slice(1);
 
+  p3.innerHTML =`<strong>Temp:</strong> ${temp}`;
 
-  if (temp >= 50 && speed >= 3.0) {
-  p2.innerHTML = `<strong>Wind Chill:</strong> ${result.toFixed(2)}&deg;F`;
-
-  } else if (temp > 50 && speed < 3.0) {
-  p2.innerHTML = `<strong>Wind Chill:</strong> ${"N/A"}`;
-  }
-
-  image.setAttribute('src', icon);
-  image.setAttribute('alt', des);
-  h6.innerHTML = description;
-  p.innerHTML = `<strong>Wind Speed:</strong> ${speed.toFixed(1)}km/h`;
-  p3.innerHTML =`<strong>Humidity:</strong> ${humidity}`;
-
-  section.append(image);
-  section.append(h6);
-  section.appendChild(p);
-  section.appendChild(p2);
   section.appendChild(p3);
 
   captionDescription.append(section);
